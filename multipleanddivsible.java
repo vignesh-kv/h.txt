3621ade79131c62b56e7e898acbfe092f2ddc60f
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-31 19:42:29 +0530

    Add files via upload

---
+++ multipleanddivsible.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class multipleanddivsible{
+public static void main(String[]args){
+System.out.println("Enter the number:");
+Scanner sc=new Scanner(System.in);
+int A=sc.nextInt();
+if(A%27==0){
+System.out.println("it is multiple of 27");
+}else{
+System.out.println("it is not multiple of 27");
+}
+}
+}
