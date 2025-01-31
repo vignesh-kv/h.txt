7c8670e5afe4e539fffbfd9debe4b059a14f1100
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-31 19:43:04 +0530

    Add files via upload

---
+++ multipleornot.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class multipleornot{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int n=200;
+if(n%100==0){
+System.out.println("it is multiple of 100");
+}else{
+System.out.println("it is not multiple of 100");
+}
+}
+}
